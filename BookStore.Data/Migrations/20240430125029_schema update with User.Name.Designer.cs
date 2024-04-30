1ac3115437ebf992439712fe80aeb28937c95fd0
loicniragire <loic.niragire@gmail.com>
2024-04-30 08:55:31 -0400

    Initial data seeding complete.

--- BookStore.Data/Migrations/20240430125029_schema update with User.Name.Designer.cs
+++ BookStore.Data/Migrations/20240430125029_schema update with User.Name.Designer.cs
@@ -4,6 +4,7 @@ using Books.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookStore.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240430125029_schema update with User.Name")]
+    partial class schemaupdatewithUserName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
