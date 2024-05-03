50acbdc4ea8a52948f191bfcfc489505b1d22267
loicniragire <loic.niragire@gmail.com>
2024-05-03 14:49:31 -0400

    Working on Angular integration. Work in progress...

---
+++ BookStore.App/ClientApp/src/polyfills.ts
@@ -0,0 +1,14 @@
+
+/**
+ * This file includes polyfills needed by Angular.
+ */
+
+import 'zone.js';  // Included with Angular CLI.
+
+/**
+ * APPLICATION IMPORTS
+ * Here you can include the polyfills for additional features you may need depending on browser support,
+ * such as custom elements or ECMAScript proposals.
+ */
+
+
